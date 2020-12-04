a03b400df2fe3c817b915428b5ff2dc329b82bfa
Pirooz Wallace <PiroozWallace@outlook.com>
2020-12-03 22:04:17 -0800

    formatting GNU license

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ const questions = [
     // create the total contents for the readME
     let readContents = `# ${response.title}
 
-This project is licensed under the ${licenseBadge}(#license) .
+This project is licensed under the ${licenseBadge} .
     
 ## Description:
 ${response.description}
